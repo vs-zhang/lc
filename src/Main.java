52198624662b6969d855b8901a80d002b4d4b170
vs-zhang <vincentzhang1209@gmail.com>
2018-07-19 00:47:41 -0400

    rotate array

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        printLine();
+        divider();
         System.out.println("Two Sum:");
         int[] nums = {1, 3, 5};
         int target = 8;
@@ -10,22 +10,38 @@ public class Main {
         int[] result = s.twoSum(nums, target);
         System.out.println(Arrays.toString(result));
 
-        printLine();
+        divider();
         System.out.println("isPalindrome String:");
         String p1 = "A man, a plan, a canal: Panama";
         String p2 = "race a car";
         System.out.printf("%s is Palindrome string: %s%n", p1, s.isPalindrome(p1));
         System.out.printf("%s is Palindrome string: %s%n", p2, s.isPalindrome(p2));
 
-        printLine();
+        divider();
         System.out.println("isPalindrome Integer:");
         int x1 = 5115;
         int x2 = 55566;
         System.out.println(s.isPalindrome(x1));
         System.out.println(s.isPalindrome(x2));
+
+        divider();
+        System.out.println("remove Element:");
+        int[] nums2 = {1, 3, 5, 8};
+        int removeElement = 3;
+        s.removeElement(nums2, removeElement);
+        System.out.println(Arrays.toString(nums2));
+
+        divider();
+        System.out.println("rotate array:");
+        System.out.println("method 2:");
+        int[] numsRotateArray = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        s.rotateArray2(numsRotateArray, k);
+        System.out.println(Arrays.toString(numsRotateArray));
+
     }
 
-    public static void printLine() {
-        System.out.println("======================");
+    public static void divider() {
+        System.out.println("========================================================================");
     }
 };
--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,18 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        final HashMap<Integer, Integer> myMap = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> myMap = new HashMap<>();
         int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            myMap.put(nums[i], i);
-        }
-        for (int i = 0; i < nums.length; i++) {
-            final Integer v = myMap.get(target - nums[i]);
-            if (v != null && v > i) {
-                return new int[]{i, v};
+            int need = target - nums[i];
+            if (myMap.containsKey(need)) {
+                result[0] = myMap.get(need);
+                result[1] = i;
+                return result;
+            } else {
+                myMap.put(nums[i], i);
             }
         }
         return null;
@@ -43,5 +45,30 @@ public class Solution {
             div /= 100;
         }
         return true;
+    }
+
+    public int removeElement(int[] nums, int target) {
+        int foundIdx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != target) {
+                nums[foundIdx++] = nums[i];
+            }
+        }
+        return foundIdx;
+    }
+
+    public void rotateArray2(int[] nums, int k) {
+        for (int i = 0; i <= k; i++) {
+            nums = shiftArray(nums);
+        }
+    }
+
+    public int[] shiftArray(int[] numArray) {
+        int first = numArray[0];
+        for (int i = 0; i < numArray.length - 1; i++) {
+            numArray[i] = numArray[i+1];
+        }
+        numArray[numArray.length - 1] = first;
+        return numArray;
     }
 }
